4ead32239c75e3a19e56a629ad76019acf8d992c
jiangshanmeta <540118044@qq.com>
2022-12-11 19:36:17 +0800

    46

---
+++ src/js.JS46.24577/solution.js
@@ -0,0 +1,9 @@
+function fibonacci (n) {
+    const dp = new Array(n + 1);
+    dp[1] = 1;
+    dp[2] = 1;
+    for (let i = 3; i < n + 1; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+    return dp[n];
+}
