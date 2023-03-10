7fccd55273fccbf9ac200a07a25467ccbe60aeed
jiangshanmeta <540118044@qq.com>
2023-03-10 21:42:46 +0800

    11

---
+++ src/go-introduction.GP11.2559039/solution.go
@@ -0,0 +1,15 @@
+package main
+
+//import "fmt"
+
+/**
+ * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+ *
+ *
+ * @param a int整型 长方形的长
+ * @param b int整型 长方形的宽
+ * @return int整型
+ */
+func perimeter(a int, b int) int {
+	return 2 * (a + b)
+}
