9fab6a2ed783d48316cea1396e11ae35913b669a
jiangshanmeta <540118044@qq.com>
2023-03-10 21:43:05 +0800

    16

---
+++ src/go-introduction.GP16.2559304/solution.go
@@ -0,0 +1,19 @@
+package main
+
+//import "fmt"
+
+/**
+ * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+ *
+ *
+ * @param a int整型
+ * @param b int整型
+ * @return int整型一维数组
+ */
+func bitOperate(a int, b int) []int {
+	return []int{
+		a & b,
+		a | b,
+		a ^ b,
+	}
+}
