11c86e26d352f628ef07a7c8e6a51800d1eedd37
jiangshanmeta <540118044@qq.com>
2023-03-10 21:42:49 +0800

    12

---
+++ src/go-introduction.GP12.2559253/solution.go
@@ -0,0 +1,14 @@
+package main
+
+//import "fmt"
+
+/**
+ * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+ *
+ *
+ * @param f double浮点型 华氏温度
+ * @return double浮点型
+ */
+func temperature(f float64) float64 {
+	return (f - 32) * 5 / 9
+}
