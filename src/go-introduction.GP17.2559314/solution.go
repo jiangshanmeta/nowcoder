521f9dd7ce62aab73a4be8013c5e4c7f8633be79
jiangshanmeta <540118044@qq.com>
2023-03-10 21:43:10 +0800

    17

---
+++ src/go-introduction.GP17.2559314/solution.go
@@ -0,0 +1,14 @@
+package main
+
+//import "fmt"
+
+/**
+ * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+ *
+ *
+ * @param x int整型 参加活动人数
+ * @return bool布尔型
+ */
+func odevity(x int) bool {
+	return x%2 == 0
+}
