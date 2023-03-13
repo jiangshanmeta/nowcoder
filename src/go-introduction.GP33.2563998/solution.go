57f59c5d3478996f90a25a9b05e7a2aca4dfa5d8
jiangshanmeta <540118044@qq.com>
2023-03-13 23:26:06 +0800

    33

---
+++ src/go-introduction.GP33.2563998/solution.go
@@ -0,0 +1,14 @@
+package main
+
+//import "fmt"
+
+/**
+ * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+ *
+ *
+ * @param hight double浮点型 身高
+ * @return bool布尔型
+ */
+func ispay(hight float64) bool {
+	return hight >= 160
+}
