80189f012ff12d0a20901c9b14ea911092b0d076
jiangshanmeta <540118044@qq.com>
2023-03-14 22:22:56 +0800

    40

---
+++ src/go-introduction.GP40.2563841/solution.go
@@ -0,0 +1,17 @@
+package main
+
+//import "fmt"
+
+/**
+ * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+ *
+ *
+ * @param x int整型
+ * @return int整型
+ */
+func absfunc(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
