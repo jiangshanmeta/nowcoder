397b90c9f4eed078bba7e137ce663763d09a65d3
jiangshanmeta <540118044@qq.com>
2024-04-24 20:27:51 +0800

    1

---
+++ src/java.JAVA11.2133839/solution.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        int m = console.nextInt();
+        int n = console.nextInt();
+        int result = getCM(m, n);
+        System.out.println(result);
+    }
+
+    public static int getCM(int m, int n) {
+        return m * n / gcd(m, n);
+    }
+
+    public static int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return gcd(b, a % b);
+    }
+}
