0aea8b22dbfc8f8f3dd7e804e112cb14e1e21d6c
jiangshanmeta <540118044@qq.com>
2024-04-29 20:29:47 +0800

    1

---
+++ src/java.JAVA23.2114415/solution.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()) {
+            String className = scanner.next();
+            // print就是需要你定义的方法
+            print(Class.forName(className).newInstance());
+        }
+    }
+
+    public static void print(Object o) {
+        System.out.println(o);
+    }
+
+}
+
+class First {
+    public String toString() {
+        return "First";
+    }
+}
+
+class Second {
+    public String toString() {
+        return "Second";
+    }
+}
+
+class Third {
+    public String toString() {
+        return "Third";
+    }
+}
